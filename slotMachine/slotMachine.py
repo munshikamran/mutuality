d5209cb787c58228aab3f1ccc15dc0c3a897b67f
Jeff <jeffrey.m.ames@gmail.com>
2012-09-08 14:24:12 -0700

    added lock and spin buttons to SlotMachine class

--- slotMachine/slotMachine.py
+++ slotMachine/slotMachine.py
@@ -12,13 +12,31 @@ class SlotMachine:
 	profile = None
 	leftSlot = None
 	rightSlot = None
+	leftSlotLocked = False
+	rightSlotLocked = False
 	def __init__(self,Profile):
 		self.profile = Profile
 		self.spinBothSlots()
 
+	#public methods
+	def spinButtonPressed(self):
+		if self.leftSlotLocked and not self.rightSlotLocked:
+			self.spinRightSlot()
+		elif self.rightSlotLocked and not self.leftSlotLocked:
+			self.spinLeftSlot()
+		elif self.rightSlotLocked and self.leftSlotLocked:
+			self.spinBothSlots()
+
+	def leftLockButtonPressed(self):
+		self.leftSlotLocked = not self.leftSlotLocked
+
+	def rightLockButtonPressed(self):
+		self.rightSlotLocked = not self.rightSlotLocked
+
 	def printState(self):
 		print self.leftSlot['name'] + ' and ' + self.rightSlot['name']
 
+	#private
 	def spinLeftSlot(self):
 		if self.rightSlot == None:
 			self.leftSlot = self.profile.getRandomFriend()
