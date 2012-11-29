f580c574e4afe6058090706f342357ed90a2de55
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2012-11-28 22:48:42 -0800

    added a FacebookUserMatch model

--- connect/models/facebookUserMatch.py
+++ connect/models/facebookUserMatch.py
@@ -1,1 +1,18 @@
-__author__ = 'jeff'
+from django.db import models
+from profile import Profile
+from facebookuser import FacebookUser
+
+
+# Create your models here.
+class FacebookUserMatch( models.Model ):
+    profile = models.ForeignKey(Profile)
+    facebookUser1 = models.ForeignKey(FacebookUser, related_name="match_facebookUser1")
+    facebookUser2 = models.ForeignKey(FacebookUser, related_name="match_facebookUser2")
+    date_created = models.DateTimeField( "Date Created", auto_now_add=True )
+    date_updated = models.DateTimeField( "Date Updated", auto_now=True )
+
+    class Meta:
+        app_label = 'connect'
+
+    def __unicode__(self):
+        return "%s : %s and %s" % ( self.profile, self.facebookUser1,self.facebookUser2 )
