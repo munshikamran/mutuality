1a56163a264483b3d74d53e1d828d95e9f32636c
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2012-11-29 20:00:36 -0800

    added match reason enums

--- common/enums/matchReasons.py
+++ common/enums/matchReasons.py
@@ -1,1 +1,12 @@
-__author__ = 'jeff'
+class MATCH_REASONS():
+
+    TOO_SMART = "TOO_SMART"
+    TOO_CRAZY = "TOO_CRAZY"
+    TOO_FAR = "TOO_FAR"
+
+
+    ENUM = (
+    (TOO_SMART,"too smart"),
+    (TOO_CRAZY,"too crazy"),
+    (TOO_FAR,"too far"),
+    )
