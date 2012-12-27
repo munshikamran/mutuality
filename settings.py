98abf2ec950cadb2015b719dcb823d69f1450047
Varun Sobti <varun.sobti@kraffsoft.com>
2012-12-27 15:36:06 +0530

    url modified

--- settings.py
+++ settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'Mutuality.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
 	os.path.join(os.path.dirname(__file__), ''),
