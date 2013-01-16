015f602800575ee483da4e149ab4ed54b54e3a58
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-15 18:35:13 -0800

    added a date created field to UserFavorite model

--- connect/models/userFavorite.py
+++ connect/models/userFavorite.py
@@ -4,6 +4,7 @@ from facebookuser import FacebookUser
 class UserFavorite( models.Model ):
     user = models.ForeignKey('Profile', related_name="favorite_of" )
     favorite = models.ForeignKey(FacebookUser, related_name="favorite_person")
+    date_created = models.DateTimeField( "Date Created", auto_now_add=True )
 
     class Meta:
         app_label = 'connect'
