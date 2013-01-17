2378a3a0dd8211fdb97e84ba3dff1571e67c2761
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-16 21:52:03 -0800

    fixed bug where it was returning Friendship instead of FacebookUser objects in GetFriendsOfFriends

--- connect/functions/getFriendsOfFriends.py
+++ connect/functions/getFriendsOfFriends.py
@@ -4,6 +4,11 @@ from getFriendList import GetFriendIDs
 
 def GetFriendsOfFriends(profile):
     friendIDs = GetFriendIDs(profile)
-    friendsOfFriends = Friendship.objects.filter(user__in=friendIDs).exclude(friend__in=friendIDs + [profile.facebookID])
-    return friendsOfFriends
+    friendshipsOfFriends = list(Friendship.objects.filter(user__in=friendIDs).exclude(friend__in=friendIDs+[profile.facebookID]).values_list('friend'))
+    friendsOfFriendsIDs = []
+    for friendship in friendshipsOfFriends:
+        friendsOfFriendsIDs.append(friendship[0])
+
+    friendsOfFriendsList = FacebookUser.objects.filter(facebookID__in=friendsOfFriendsIDs)
+    return friendsOfFriendsList
 
