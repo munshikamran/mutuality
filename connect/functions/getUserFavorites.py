c27f2fd611a50f978c5ca91975367c22ef58068d
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-20 20:12:32 -0800

    GetUserFavorites now returns facebook user objects instead of UserFavorite objects

--- connect/functions/getUserFavorites.py
+++ connect/functions/getUserFavorites.py
@@ -1,10 +1,15 @@
 from connect.models import UserFavorite
+from connect.models import FacebookUser
 
 def GetUserFavorites(profile):
 #    get favorites for this user. Order so that most recently favorited users appear first in array
     try:
-        favoritesOfProfile = UserFavorite.objects.filter(user=profile).order_by('date_created').reverse()
-        return favoritesOfProfile
+        favoritesOfProfile = UserFavorite.objects.select_related('favorite').filter(user=profile).order_by('date_created').reverse()
+        facebookUsers = []
+        for favorite in favoritesOfProfile:
+            facebookUsers.append(favorite.favorite)
+
+        return facebookUsers
     except:
         print "Unexpected error when fetching user favorites"
         return False
