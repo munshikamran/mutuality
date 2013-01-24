a198d5bcea858a7df563de21be677970d20b47e0
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-23 21:13:52 -0800

    removed debugging in viewUser.py

--- connect/functions/viewUser.py
+++ connect/functions/viewUser.py
@@ -3,7 +3,6 @@ from connect.models import UserViewed,FacebookUser
 def ViewUser(profile,facebookUserID):
     try:
         facebookUser = FacebookUser.objects.get(facebookID=facebookUserID)
-        print facebookUser
         userViewed, created = UserViewed.objects.get_or_create(user=profile, viewed=facebookUser)
         userViewed.save()
         return True
