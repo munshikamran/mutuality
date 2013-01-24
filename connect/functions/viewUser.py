5e93d371e8c7bb0d989f350359a5118b2ff13787
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-23 21:35:48 -0800

    the function ViewUser now takes in the filter value

--- connect/functions/viewUser.py
+++ connect/functions/viewUser.py
@@ -1,9 +1,9 @@
 from connect.models import UserViewed,FacebookUser
 
-def ViewUser(profile,facebookUserID):
+def ViewUser(profile,facebookUserID,filter):
     try:
         facebookUser = FacebookUser.objects.get(facebookID=facebookUserID)
-        userViewed, created = UserViewed.objects.get_or_create(user=profile, viewed=facebookUser)
+        userViewed, created = UserViewed.objects.get_or_create(user=profile, viewed=facebookUser, filter=filter)
         userViewed.save()
         return True
     except:
