8311c4525c00ccd78c90a42b07655b23a048e1e3
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-01-31 22:49:35 -0800

    now dating filter works for GetMeetPeople function

--- connect/functions/getFriendsOfFriends.py
+++ connect/functions/getFriendsOfFriends.py
@@ -1,7 +1,7 @@
 from connect.models import Friendship
 from connect.models import FacebookUser
 from getFriendList import GetFriendIDs
-
+from common.enums import RELATIONSHIP_STATUS
 def GetFriendsOfFriends(profile):
     friendIDs = GetFriendIDs(profile)
     friendshipsOfFriends = list(Friendship.objects.filter(user__in=friendIDs).exclude(friend__in=friendIDs+[profile.facebookID]).values_list('friend'))
@@ -15,4 +15,12 @@ def GetFriendsOfFriends(profile):
 def GetFriendsOfFriendsInArea(profile):
     fofInArea = GetFriendsOfFriends(profile).filter(state = profile.state)
     return fofInArea
+
+def GetFriendsOfFriendsSingleInArea(profile):
+    fofInArea = GetFriendsOfFriendsInArea(profile)
+    genderToExclude = profile.gender
+    relationshipStatusesToExclude = [RELATIONSHIP_STATUS.RELATIONSHIP,RELATIONSHIP_STATUS.ENGAGED,RELATIONSHIP_STATUS.RELATIONSHIP]
+    fofSingleInArea = fofInArea.exclude(gender=genderToExclude).exclude(relationshipStatus__in=relationshipStatusesToExclude)
+    return fofSingleInArea
+
 
