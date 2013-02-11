6a6088b75eff37487d500435e71848628330360f
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-02-10 18:35:28 -0800

    -rewrote SendMessage function to work with new Message model -GetMessageThreadWithOther now takes in a facebookID for the other user instead of a profile object -GetMessageThreadWithOther now marks received messages as read when called -Created a function GetNewMessageCount which returns the number of unread messages a user has

--- connect/functions/getMessages.py
+++ connect/functions/getMessages.py
@@ -1,6 +1,12 @@
 from messages.models import Message
-from datetime import datetime, timedelta
+from connect.models import Profile
+from datetime import datetime
 from django.db.models import Q
+
+
+def GetNewMessageCount(profile):
+    receivedMessageCount = Message.objects.filter(recipient=profile).filter(read_at__isnull=True).count()
+    return receivedMessageCount
 
 # Thread preview functions return an array of Message objects. See messages.models for a full description of message
 # objects. For quick reference message.sender and message.recipient are Profile objects. Thus message.sender.facebookID
@@ -23,9 +29,17 @@ def GetThreadPreviewsSince(profile,pastDate):
     return threadPreviews
 
 
-def GetMessageThreadWithOther(profile,otherProfile):
+def GetMessageThreadWithOther(profile,otherUserFacebookID):
+    otherProfile = Profile.objects.get(facebookID=otherUserFacebookID)
     q =  Q(sender_deleted_at__isnull=True) & (Q(sender=profile) & Q(recipient=otherProfile)) | (Q(sender=otherProfile) & Q(recipient=profile))
     #order older messages first
+    messages = Message.objects.filter(q).order_by('sent_at')
+    # once these messages are returned we mark them as read
+    now = datetime.now()
+    for message in messages:
+        if not message.read_at and profile == message.recipient:
+            message.read_at = now
+            message.save()
     return Message.objects.filter(q).order_by('sent_at')
 
 #private
