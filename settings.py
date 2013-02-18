3068450d43fd8a1b9cb132a0569b8b63ba263778
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-02-18 10:34:55 -0800

    updated settings.py for herouku deployment

--- settings.py
+++ settings.py
@@ -31,18 +31,20 @@ if DEVELOPMENT_MODE:
     URL = "http://localhost:8000"
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'mutuality',                      # Or path to database file if using sqlite3.
-            'USER': 'root',                      # Not used with sqlite3.
-            'PASSWORD': 'redhat',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
+}
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+    URL = "boiling-ridge-8911.herokuapp.com"
     FACEBOOK_APP_ID = '475217095841801'
     FACEBOOK_APP_SECRET = '1304979d3d82251c8dd383e179c30126'
-    URL = "http://mymutuality.com"
 
 
 # Local time zone for this installation. Choices can be found here:
