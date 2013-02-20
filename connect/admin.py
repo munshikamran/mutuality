d5513ac829063b3d5174698a81fa441f16a4a0bb
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-02-19 21:53:59 -0800

    updated connect admin to show Profile location field

--- connect/admin.py
+++ connect/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.conf import settings
 from connect.models import Profile
 
-if settings.DEBUG:
-    admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+
+
+admin.site.register(Profile,ProfileAdmin)
