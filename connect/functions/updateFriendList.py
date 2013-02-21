8e9ce5a4f7ea4d12babc50e381cb29be4c5a4c9d
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-02-20 23:21:28 -0800

    added kwargs input to UpdateFriendList function for setting limit and offset

--- connect/functions/updateFriendList.py
+++ connect/functions/updateFriendList.py
@@ -2,9 +2,16 @@ from connect.models import Profile,Friendship,FacebookUser
 from getProfileAuthToken import GetProfileAuthToken
 import facebook
 
-def UpdateFriendList(profile):
+# use kwargs to set friendlist limits and offsets for updating parts of the friend list. this can be used for quickly
+# updating a subset of the friendlist when time is of the essence and the entire friendlist isn't needed.
+# Note: if kwargs is empty then it updates the entire friendlist with no limit
+# e.g. 1 update friendlist with at most 50 of the user's friends):
+# UpdateFriendList(profile,**{"limit" : 50})
+# e.g. 2 update friendlist with limit and offset
+# UpdateFriendList(profile, **{limit" : 10, "offset" : 10})
+def UpdateFriendList(profile,**kwargs):
     try:
-        friendListData = getFriendListFromFacebook(profile)
+        friendListData = getFriendListFromFacebook(profile,**kwargs)
         for friend in friendListData:
             facebookUser = createOrUpdateFacebookUser(friend)
             friendship = createOrUpdateFriendShip(profile,facebookUser)
@@ -12,10 +19,11 @@ def UpdateFriendList(profile):
     except:
         return False
 
-def getFriendListFromFacebook(profile):
+def getFriendListFromFacebook(profile,**kwargs):
     graph = facebook.GraphAPI(GetProfileAuthToken(profile))
     fields = ['name','location','picture','gender','birthday','relationship_status']
-    kwargs = {"fields": fields}
+    kwargs['fields'] = fields
+    # kwargs = {"fields": fields, "limit" : limit}
     friendList = graph.get_connections("me","friends",**kwargs)['data']
     return friendList
 
