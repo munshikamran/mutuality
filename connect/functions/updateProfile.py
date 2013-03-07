98e1d10c219bddf8c07edd5e0ee767ef0908153e
Jeffrey Ames <jeff@Jeffreys-MacBook-Pro.local>
2013-03-06 21:38:11 -0800

    added UpdateProfile function for registration page

---
+++ connect/functions/updateProfile.py
@@ -0,0 +1,10 @@
+from connect.models import Profile
+
+# user data is a dictionary which can contain any of the following key value pairs
+# 'name' : name of user
+# 'birthdayString' : e.g. '12/30/1984'
+# 'location' : <city>,<state> e.g. 'Seattle, Washington'
+#  'relationship_status' : one of the values in common.enums.relationship_status.py
+def UpdateProfile(profile,userData):
+    profile.updateUsingFacebookDictionary()
+    profile.save()
