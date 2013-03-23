134cb8294c79730e8c71fea8dd6400dff5e20da8
Kamran Munshi <kamran.munshi@gmail.com>
2013-03-23 13:18:19 -0700

    Registration page

--- static/javascripts/account.js
+++ static/javascripts/account.js
@@ -1,60 +1,60 @@
 
 (function($) {
 
-		// Initialize ajax autocomplete:
-        $('#reg-location').autocomplete({
-            serviceUrl: 'https://graph.facebook.com/search?type=adcity&limit=5&country_list=%5B"us"%5D',
-            onSelect: function(suggestion) {
-                $('#location-ajax').html(suggestion.value);
+	// Initialize ajax autocomplete:
+	$('#reg-location').autocomplete({
+		serviceUrl: 'https://graph.facebook.com/search?type=adcity&limit=5&country_list=%5B"us"%5D',
+		onSelect: function(suggestion) {
+		    $('#location-ajax').html(suggestion.value);
 
-            },
-            deferRequestBy: 10,
-            autoSelectFirst: true,
-            onSearchStart: function (query) {$('#location-ajax').html("");},
-	        paramName: 'q',
-	        transformResult: function(response, originalQuery) {
-	            return {
-	                query: originalQuery,
-	                suggestions: $.map(JSON.parse(response).data, function(dataItem) {
-	                    return { value: dataItem.name, data: dataItem.name };
-	                })
-	            };
-	        }
-   	 	});
+		},
+		deferRequestBy: 10,
+		autoSelectFirst: true,
+		onSearchStart: function (query) {$('#location-ajax').html("");},
+		paramName: 'q',
+		transformResult: function(response, originalQuery) {
+		    return {
+		        query: originalQuery,
+		        suggestions: $.map(JSON.parse(response).data, function(dataItem) {
+		            return { value: dataItem.name, data: dataItem.name };
+		        })
+		    };
+		}
+	});
   
 /* Begin Helper functions */
-        //Alert message stuff
-		var myMessages = ['error','success']; // define the messages types		 
-		var hideAllMessages = function() {
-			 var messagesHeights = new Array(); // this array will store height for each
-			 for (i=0; i<myMessages.length; i++) {
-					  messagesHeights[i] = $('.' + myMessages[i]).outerHeight();
-					  $('.' + myMessages[i]).css('top', -messagesHeights[i]); //move element outside viewport	  
-			 }
-		}
+	//Alert message stuff
+	var myMessages = ['error','success']; // define the messages types		 
+	var hideAllMessages = function() {
+		 var messagesHeights = new Array(); // this array will store height for each
+		 for (i=0; i<myMessages.length; i++) {
+				  messagesHeights[i] = $('.' + myMessages[i]).outerHeight();
+				  $('.' + myMessages[i]).css('top', -messagesHeights[i]); //move element outside viewport	  
+		 }
+	}
 
-		var showMessage = function(type){
-			$('.'+ type +'-trigger').click(function(){
-				  hideAllMessages();				  
-				  $('.'+type).animate({top:"0"}, 500);
-			});
-		}
+	var showMessage = function(type){
+		$('.'+ type +'-trigger').click(function(){
+			  hideAllMessages();				  
+			  $('.'+type).animate({top:"0"}, 500);
+		});
+	}
 /* End Helper functions */
 
 /* Begin Account Main Code */
 
+	// Initially, hide them all
+	hideAllMessages();
 
-		 // Initially, hide them all
-		 hideAllMessages();
-		 // Show message
-		 for(var i=0;i<myMessages.length;i++){
-			showMessage(myMessages[i]);
-		 }
-		 
-		 // When message is clicked, hide it
-		 $('.message').click(function(){			  
-				  $(this).animate({top: -$(this).outerHeight()}, 500);
-		  });	
+	// Show message
+	for(var i=0;i<myMessages.length;i++){
+		showMessage(myMessages[i]);
+	}
+
+	// When message is clicked, hide it
+	$('.message').click(function(){			  
+		  $(this).animate({top: -$(this).outerHeight()}, 500);
+	});	
 
 
 	if(Mutuality.cache.profile == 'female'){
@@ -76,6 +76,10 @@
 			console.log(profileDict);
 			Mutuality.setProfile(profileDict['location'], profileDict['relationship_status'], profileDict['gender'], function(response){
 				$('.success-trigger').trigger('click');
+	            var url = window.location.href; 
+				if(url.indexOf("register") !== -1) { 
+					window.location = "/meetpeople/";
+				}
 			});
 		}
 		else{
