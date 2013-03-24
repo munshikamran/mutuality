602b49761aa59947b7d80b5ce89c5c4eb398224c
Jeff <jeffrey.m.ames@gmail.com>
2013-03-24 15:45:30 -0700

    added separate settings files for different server environments

---
+++ settings/staging.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dummy.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+ }
+FACEBOOK_APP_ID = '503402936389228'
+FACEBOOK_APP_SECRET = '00c92e60a39c18b6ead3f69dba7aa6f1'
+URL = "http://localhost:8000"
