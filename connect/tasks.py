ebde065c6da7e0bb8ae7cd79aae60bd680e9e7bb
Jeff <jeffrey.m.ames@gmail.com>
2013-03-25 23:01:40 -0700

    test

--- connect/tasks.py
+++ connect/tasks.py
@@ -1,11 +1,12 @@
 from celery import task
-# from time import sleep
-# from django.core.mail import send_mail
+from time import sleep
+from django.core.mail import send_mail
 
 @task()
 def add(x, y):
     return x+y
 
 @task()
-def sendEmail():
+def sendEmail(sleepTime):
+	sleep(sleepTime)
 	return send_mail('Subject here', 'Here is the message.', 'from@example.com', ['jeffreymames@gmail.com'], fail_silently=False)
