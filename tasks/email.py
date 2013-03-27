a2e48c55f7088d0064670e6170958526543ff13b
Jeff <jeffrey.m.ames@gmail.com>
2013-03-26 22:09:54 -0700

    test

--- tasks/email.py
+++
@@ -1,8 +0,0 @@
-from celery import task
-from django.core.mail import send_mail
-from time import sleep
-
-@task()
-def sendEmail(sleepTime):
-	sleep(sleepTime)
-	return send_mail('Subject here', 'Here is the message.', 'from@example.com', ['jeffreymames@gmail.com'], fail_silently=False)
