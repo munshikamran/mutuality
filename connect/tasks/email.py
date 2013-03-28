37dc781f68be386c1172591a16c9cc593a4a044d
Jeff <jeffrey.m.ames@gmail.com>
2013-03-27 20:59:23 -0700

    new user email function

--- connect/tasks/email.py
+++ connect/tasks/email.py
@@ -1,8 +1,9 @@
 from celery import task
 from django.core.mail import send_mail
-from time import sleep
 
-@task()
-def sendEmail(sleepTime):
-	sleep(sleepTime)
-	return send_mail('Subject here', 'Here is the message.', 'from@example.com', ['jeffreymames@gmail.com'], fail_silently=False)
+@task
+def send_user_joined_email(profile):
+	recipients = ['jeffreymames@gmail.com', 'jazjit.singh@gmail.com', 'kamran.munshi@gmail.com']
+	subject = 'A New User Joined Mutuality!'
+	message = '{0} joined Mutuality'.format(profile.name)
+
