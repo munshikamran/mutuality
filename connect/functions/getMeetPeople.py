087ed069b8db41e0221876768efb8b4ac2bb1e0e
Jeff Ames <jeffrey.m.ames@gmail.com>
2013-03-31 23:06:35 -0700

    added dating filter to GetMeetPeople

--- connect/functions/getMeetPeople.py
+++ connect/functions/getMeetPeople.py
@@ -10,6 +10,7 @@ from connect.functions.createPotentialBatch import CreatePotentialBatch
 from connect.models import PotentialBatch
 from connect.models import PotentialMatch
 from datetime import datetime
+from common.enums import RELATIONSHIP_STATUS
 
 # def GetMeetPeople(profile, filter):
 #
@@ -57,6 +58,10 @@ def GetMeetPeople(profile, filter):
         CreatePotentialBatch(profile)
     batch = PotentialBatch.objects.filter(profile=profile).order_by('-date_created')[0]
     potentialMatches = PotentialMatch.objects.filter(potentialMatchBatch=batch).select_related('facebookUser')
+    if filter == MEET_PEOPLE_FILTER.DATING:
+        genderToExclude = profile.gender
+        relationshipStatusesToExclude = [RELATIONSHIP_STATUS.RELATIONSHIP,RELATIONSHIP_STATUS.ENGAGED,RELATIONSHIP_STATUS.MARRIED]
+        potentialMatches = potentialMatches.exclude(facebookUser__gender=genderToExclude, facebookUser__relationshipStatus__in=relationshipStatusesToExclude)
     # TODO filter out users who have been seen
     viewedUsers = []
     freshUsers = []
