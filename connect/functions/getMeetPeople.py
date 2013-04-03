28bd53a56da97c945850fc6f12dcb648c507aac2
Jeff Ames <jeffrey.m.ames@gmail.com>
2013-04-02 21:52:43 -0700

    check if user has friends on the site when making meet people call

--- connect/functions/getMeetPeople.py
+++ connect/functions/getMeetPeople.py
@@ -1,18 +1,25 @@
 from connect.classes import MeetPeopleResponse
-
 from common.enums import MEET_PEOPLE_FILTER
 from connect.functions.createPotentialBatch import CreatePotentialBatch
 from connect.models import PotentialBatch
 from connect.models import PotentialMatch
+from connect.models.friendship import Friendship
 from datetime import datetime
 from common.enums import RELATIONSHIP_STATUS
 
 
 def GetMeetPeople(profile, filter):
+    # check if the users friend list has been uploaded
+    if not Friendship.objects.filter(user=profile).exists():
+        return "Your friendlist has not been uploaded yet"
+
     if not PotentialBatch.objects.filter(profile=profile, date_expiration__gt=datetime.now()).exists():
         CreatePotentialBatch(profile)
     batch = PotentialBatch.objects.filter(profile=profile).order_by('-date_created')[0]
     potentialMatches = PotentialMatch.objects.filter(potentialMatchBatch=batch).select_related('facebookUser')
+    if not potentialMatches.exists():
+        return "We cannot find any matches for you"
+    
     if filter == MEET_PEOPLE_FILTER.DATING:
         genderToExclude = profile.gender
         relationshipStatusesToExclude = [RELATIONSHIP_STATUS.RELATIONSHIP, RELATIONSHIP_STATUS.ENGAGED, RELATIONSHIP_STATUS.MARRIED]
