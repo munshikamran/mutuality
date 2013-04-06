a13520b763a7123d18cd9c51af351f394cd8d364
Jeff Ames <jeffrey.m.ames@gmail.com>
2013-04-06 16:20:21 -0700

    GetMeetPeopleResponse now returns message and empty arrays when no matches found

--- connect/classes/meetPeopleResponse.py
+++ connect/classes/meetPeopleResponse.py
@@ -1,8 +1,9 @@
 class MeetPeopleResponse:
-    def __init__(self,freshUsers,viewedUsers):
+    SUCCESS_MESSAGE = 'success'
+    SEEN_ALL_MATCHES_MESSAGE = 'you have seen all potential matches'
+    NO_FRIENDS_MESSAGE = 'you have no friends on the site'
+    UNKNOWN_ERROR_MESSAGE = 'an unknown error occurred'
+    def __init__(self, potentialMatches, message):
 #        new users are ordered by number of mutual friends. The user with the most mutual friends with
 #        the requesting user is first in the list
-        self.freshUsers = freshUsers
-#        viewed users are ordered by how recently the user was viewed. The user who was viewed most
-#        recently by the requesting user is first in the list
-        self.viewedUsers = viewedUsers
+        self.potentialMatches = potentialMatches
