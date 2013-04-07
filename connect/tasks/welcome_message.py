b3d61464b05ca93b1e5372c57cd18c003d7762ca
Jeff Ames <jeffrey.m.ames@gmail.com>
2013-04-06 19:23:57 -0700

    fixed bug in send welcome message function

--- connect/tasks/welcome_message.py
+++ connect/tasks/welcome_message.py
@@ -7,7 +7,7 @@ def send_welcome_message(profile):
 	founder_ids = ['1240230066', '3900450', '1065870234']
 	founder_profiles = Profile.objects.filter(facebookID__in=founder_ids)
 	# get ransdm founder
-	idx = random.randint(0, len(founder_profiles)-1)
+	idx = randint(0, len(founder_profiles)-1)
 	sender = founder_profiles[idx]
 	first_name = sender.name.split(' ')[0]
 	message = "Welcome! My name is {0} and I am one of the co-founders of Mutuality. I hope you enjoy the site and please stay in touch if you have any questions.".format(first_name)
