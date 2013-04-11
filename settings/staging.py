f837ad72107282de5d857302a7575afcebc72cb8
Jeffrey Ames <jeff@Jeffrey-Ames-iMac.local>
2013-04-11 15:34:02 -0700

    fixed settings files

--- settings/staging.py
+++ settings/staging.py
@@ -14,6 +14,6 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 URL = 'www.mymutuality.com'
 
-# celery
-BROKER_URL = 'amqp://nfhtezck:VA2WoDvlivIMoZnISotQzFFZS9F16tv6@tiger.cloudamqp.com/nfhtezck'
-BROKER_POOL_LIMIT = None
+# # celery
+# BROKER_URL = 'amqp://nfhtezck:VA2WoDvlivIMoZnISotQzFFZS9F16tv6@tiger.cloudamqp.com/nfhtezck'
+# BROKER_POOL_LIMIT = None
