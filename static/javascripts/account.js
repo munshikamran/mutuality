86d8da9a92bd6a1590b0a59cef384613d8b9615e
Jasjit Singh <jazjit.singh@gmail.com>
2013-04-12 15:00:21 -0700

    mixpanel registration fix, I think

--- static/javascripts/account.js
+++ static/javascripts/account.js
@@ -88,9 +88,10 @@
 				$('.success-trigger').trigger('click');
 	            var url = window.location.href; 
 				if(url.indexOf("register") !== -1) { 
+					mixpanel.track("Registration");
+					console.log("Registration Complete");
 					mixpanel.alias(Mutuality.cache.profile.facebookID);
 					mixpanel.identify(Mutuality.cache.profile.facebookID);
-					mixpanel.track("Registration");
 					window.location = "/meetpeople/";
 				}
 			});
