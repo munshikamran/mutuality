3a019736f6d8753c7324f05c1e17decd83a20c80
Jeff Ames <jeffrey.m.ames@gmail.com>
2013-04-16 23:33:36 -0700

    fixed another cyclical import problem

--- connect/tasks/__init__.py
+++ connect/tasks/__init__.py
@@ -1,3 +1,1 @@
-from connect.tasks.user_joined import user_joined
 from connect.tasks.update_potential_matches import update_all_potential_matches
-from connect.tasks.message_sent import message_sent
