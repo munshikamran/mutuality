fde20fcfc01d05c86b322c649126436753a96211
Kamran Munshi <kamran.munshi@gmail.com>
2013-04-21 23:08:59 -0700

    print statements to figure out this lastlogin shit

--- connect/views.py
+++ connect/views.py
@@ -97,7 +97,9 @@ def meetpeople(request):
     request.session.set_expiry(604800)  # one week
     if hasattr(request, 'user'):
         context_dict['user'] = request.user
+        print "CUNT BITCH"
         if "last_login_date" in request.session.keys():
+            print (datetime.now() - request.session['last_login_date']).days + " FUCKING HERE"
             if (datetime.now() - request.session['last_login_date']).days > 0:
                 print "**Updating last login!**"
                 request.session['last_login_date'] = datetime.now()
