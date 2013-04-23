13bc2fe695a0d049e0e2845a7c3e7d67578b5340
Jasjit Singh <jazjit.singh@gmail.com>
2013-04-22 21:57:40 -0700

    fixed hasLikedBeacon check so that it only checks if you have liked the most latest beacon by a user rather than just any beacon

--- connect/functions/getHasLikedBeacon.py
+++ connect/functions/getHasLikedBeacon.py
@@ -1,6 +1,7 @@
 from connect.models.beaconUserLike import BeaconUserLike
-
+from connect.models.beacon import Beacon
 
 def hasLikedBeacon(myProfile, theirFbID):
-    beaconLikes = BeaconUserLike.objects.filter(user=myProfile, beacon__user_id=theirFbID).exists()
+    beacons = Beacon.objects.filter(user=theirFbID).latest('date_created').id
+    beaconLikes = BeaconUserLike.objects.filter(user=myProfile, beacon_id=beacons).exists()
     return beaconLikes
