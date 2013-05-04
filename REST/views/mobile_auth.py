ad2fe9152ba9bab6776df332d2f6be937efb0a7c
Colin <colin@colin-lenovo.(none)>
2013-05-04 18:08:44 -0500

    moved to token-based auth according to rest framework docs

--- REST/views/mobile_auth.py
+++ REST/views/mobile_auth.py
@@ -3,13 +3,24 @@ from la_facebook.access import OAuthAccess
 from la_facebook.access import OAuth20Token
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 import django.utils.simplejson as json
 import urllib
 import cgi
 from la_facebook.callbacks.default import DefaultFacebookCallback
 from django.contrib.auth.models import User
+from django.middleware import csrf
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.core.signals import request_finished
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 
 class MobileAuthAPI(APIView):
+
+    permission_classes = (AllowAny,);
 
     def post(self, request, format=None):
         
@@ -18,7 +29,7 @@ class MobileAuthAPI(APIView):
             logger.debug("mobileauth invoked with user %s on session" % request.user);
             if request.user.is_authenticated():
                 logger.debug("mobileauth invoked with authenticated user on session");
-                return Response(user.id,status=200);
+                return Response(None,status=204);
 
         #validate the access token by hitting graph.facebook.com
         fbAuthToken = request.DATA['access_token'];
@@ -30,17 +41,17 @@ class MobileAuthAPI(APIView):
         if fbResponseCode <> 200:
             logger.debug("failed to verify access token. response code: %s. message: %s" % (fbResponseCode, raw_data));
             return Response("failed to verify access token", status=500);
-        response = json.loads(raw_data);
-        fbUserId = response['id'];
+        fbResponse = json.loads(raw_data);
+        fbUserId = fbResponse['id'];
         
         #let an instance of the LaFacebook default callback handle Django auth
         lafbCallback = DefaultFacebookCallback()
-        existing_user = lafbCallback.lookup_user(None, None, response);
+        existing_user = lafbCallback.lookup_user(None, None, fbResponse);
         logger.debug("existing user? %s" % existing_user);
         if existing_user is None:
             #create user record
             access = OAuthAccess()
-            existing_user = lafbCallback.create_user(request._request, access, OAuth20Token(fbAuthToken), response);
+            existing_user = lafbCallback.create_user(request._request, access, OAuth20Token(fbAuthToken), fbResponse);
         else:
             username = existing_user.username;
             logger.debug("query for user %s" % username);
@@ -48,7 +59,24 @@ class MobileAuthAPI(APIView):
             lafbCallback.login_user(request._request, userObj);
             
         #return success & the user id if everything worked
+        response = {};
+        response['id'] = fbResponse['id'];
+        logger.debug("existing_user %s", existing_user);
+        authUserPk = existing_user.id;
+        response['token'] = Token.objects.get(user=existing_user).key;
+        logger.debug("csrf dict: %s" % csrf.get_token(request));
         if existing_user.is_authenticated():
-            return Response(fbUserId, status=200);
+            return Response(response, status=200);
         else:
             return Response("Authentication failed for %s" % fbUserId, status=500);
+        
+    #register a signal to create RestFramework auth tokens when
+    #  new users are saved
+    @receiver(post_save, sender=User)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        print "received user post-save signal";
+        if created:
+            Token.objects.create(user=instance)
+
+
+
